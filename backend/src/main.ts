b5f996c592d65cb8d0dea5aef0e499da7f7a8bf9
Vinnyy Resende <viniciusvresendeo@gmail.com>
2025-12-07 18:30:06 -0300

    🐛 fix: enabling cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,8 @@ declare const module: any
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	app.enableCors({ origin: "*" })
 	await app.listen(process.env.PORT ?? 3000)
 
 	if (module.hot) {
